c581d1420283ada02dfdf2d679132e2d033fc78e
sivx76 <sivx76@gmail.com>
2022-08-15 08:09:21 -0700

    Made TableData protocol public

--- Sources/Swift Table/TableData.swift
+++ Sources/Swift Table/TableData.swift
@@ -9,7 +9,7 @@ import UIKit
 
 
 /// The protocol used to make a Struct or Class be inserted into the  ``Table/Table/dataSource`` of ``Table/Table``.
- protocol TableData {
+ public protocol TableData {
     var title: String { get }
     var subtitle: String? { get }
     var image: UIImage? { get }
