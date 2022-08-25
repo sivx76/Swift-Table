acc052515b1973ffdd025b09cf7360a435a9d782
sivx76 <sivx76@gmail.com>
2022-08-24 17:27:46 -0700

    Added files to be ignored by Package.swift for SPM

--- Package.swift
+++ Package.swift
@@ -20,6 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Table",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["Contributors Guide.rtf", "LICENSE", "Opportunties.rtf", "Tables.podspec", "Example", "Other", ".git", ".gitignore"]
+        ),
     ]
 )
